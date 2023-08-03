cdfa32a99f0162283fb848c3a9f0667de7e2acdf
MohamCoder <mohammedmus1000aplle@gmail.com>
2023-08-03 06:16:42 +0300

    lawg will not run

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,91 @@
-import Image from 'next/image'
+"use client";
+import { useState } from 'react'
+import * as tf from '@tensorflow/tfjs'
+import * as mobilenet from '@tensorflow-models/mobilenet'
 
 export default function Home() {
+  const [image, setImage] = useState(null)
+  const [result, setResult] = useState(null)
+  const [bill, setBill] = useState(getRandomInt(0, 100))
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      // Display a preview of the selected image
+      setBill(getRandomInt(0, 100))
+      setIsLoading(true)
+      setResult(null)
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setImage(e.target.result)
+      }
+      reader.readAsDataURL(file)
+
+      // Load the pre-trained MobileNet model
+      const model = await mobilenet.load()
+
+      // Create an image element to display the selected file
+      const img = document.createElement('img')
+      img.src = URL.createObjectURL(file)
+      img.onload = async () => {
+        // Convert the image to a tensor
+        const imgTensor = tf.browser.fromPixels(img)
+
+        // Classify the image using the model
+        const predictions = await model.classify(imgTensor)
+
+        // Display the top prediction
+        setResult(predictions[0].className.split(',')[0])
+
+        // Remove the img element from the DOM
+        img.remove()
+
+        setIsLoading(false)
+      }
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="p-4">
+      <label className="btn btn-primary">
+        <span>Select an image</span>
+        <input type="file" onChange={handleFileChange} className="hidden" />
+      </label>
+      {isLoading && (
+        <div className="flex ml-[50px] mt-4">
+          <span className="loading loading-spinner loading-lg"></span>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+      {image && !isLoading && (
+        <div>
+          <h2 className="text-lg font-bold mb-2 mt-4">Preview:</h2>
+          <img src={image} alt="Preview" className="rounded-md shadow-md" />
+        </div>
+      )}
+      {result && !isLoading && (
+        <div>
+          <h2 className="text-lg font-bold mb-2 mt-4">Result:</h2>
+          <p className="text-gray-700">{result}Lawg</p>
+          <p className="text-gray-700">Lawg bills: {bill}$</p>
+          <button className="btn mt-2 btn-error" onClick={() => window.my_modal_1.showModal()}>pay lawg bills</button>
+          <dialog id="my_modal_1" className="modal">
+            <form method="dialog" className="modal-box">
+              <h3 className="font-bold text-lg">well .... your card got declined</h3>
+              <p className="py-4">The bill collector is coming after you💀💀</p>
+              <div className="modal-action">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">run</button>
+              </div>
+            </form>
+          </dialog>
+        </div>
+      )}
+    </div>
   )
+}
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
